2135806c931dc43f9aabe0ce0aeb4805a999a6be
Patrick Xin <patrick.xin.dev@gmail.com>
2025-10-30 12:58:45 -0400

    fix: clean up unused files

--- app/(app)/(root)/page.tsx
+++ app/(app)/(root)/page.tsx
@@ -1,16 +1,9 @@
-import { DropdownMenuDemo } from "../../../components/examples/menubar/a";
-import ExampleMenubar1 from "../../../components/examples/menubar/d";
-import { StatefulMenubar } from "../../../components/examples/menubar/f";
-import ExampleMenubar from "../../../components/examples/menubar/menubar-demo";
+import { HeroSection } from "@/components/site/homepage/hero";
 
 export default function Home() {
   return (
     <div className="flex h-[calc(100dvh-12rem)] justify-center items-center">
-      {/* <HeroSection /> */}
-      <ExampleMenubar1 />
-      <ExampleMenubar />
-      <StatefulMenubar />
-      <DropdownMenuDemo />
+      <HeroSection />
     </div>
   );
 }
