c6a10dc931bf90e7ed5c1565bc75d679fe80164b
Patrick Xin <patrick.xin.dev@gmail.com>
2025-12-27 13:49:48 -0500

    fix: remove unused components

--- apps/www/app/(app)/(root)/page.tsx
+++ apps/www/app/(app)/(root)/page.tsx
@@ -1,11 +1,9 @@
-import ExampleCreatableComboboxA from "../../../components/examples/ui/combobox/a";
-import ExampleCreatableCombobox from "../../../components/examples/ui/combobox/b";
+import { HeroSection } from "@/components/site/homepage/hero";
 
 export default function Home() {
   return (
     <div className="flex h-[calc(100dvh-12rem)] justify-center items-center">
-      <ExampleCreatableCombobox />
-      <ExampleCreatableComboboxA />
+      <HeroSection />
     </div>
   );
 }
