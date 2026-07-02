d1a7e55d7dd9b9c8b3fe8d2f7eaf7c0d60df3f7e
Patrick Xin <patrick.xin.dev@gmail.com>
2026-07-02 06:21:07 -0400

    chore: bump base-ui, style fix for select

--- packages/ui/src/components/ui/select.tsx
+++ packages/ui/src/components/ui/select.tsx
@@ -324,7 +324,8 @@ function SelectContent({
             "bg-popover text-popover-foreground rounded-md shadow-md overflow-hidden",
             "outline outline-border dark:-outline-offset-1 animate-popup",
             "[&:not([data-side=none])]:max-h-(--available-height) min-w-(--anchor-width)",
-            "data-[side=none]:data-ending-style:transition-none data-[side=none]:data-starting-style:transition-none data-[side=none]:data-starting-style:scale-100 data-[side=none]:data-starting-style:opacity-100 data-[side=none]:min-w-(--anchor-width+0.3rem)",
+            "data-[side=none]:data-ending-style:transition-none data-[side=none]:data-starting-style:transition-none data-[side=none]:data-starting-style:scale-100 data-[side=none]:data-starting-style:opacity-100",
+            "data-[side=none]:min-w-[calc(var(--anchor-width)+var(--spacing))]",
             className,
           )}
           data-slot="select-content"
